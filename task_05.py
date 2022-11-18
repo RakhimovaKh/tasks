a30504df166e04c62486b1eb43a7f279502da5e4
Nikita Pyshkov <85805562+pyshkovni@users.noreply.github.com>
2022-11-18 23:29:14 +0300

    Выполнено

--- task_05.py
+++ task_05.py
@@ -7,3 +7,17 @@ for month in range(12):
     credit += need + (need * round(percent, 2))
     percent += 0.03
 print(f'Необходимо взять в долг {credit} рублей')
+
+# Хорошо. 
+# Вариант через while
+# Решение 2
+salary, expenses = 10000, 12000
+
+i = expenses - salary
+m = 0
+debt = 0
+while m < 12:
+    mx = i * (1.03 ** m)
+    debt += mx
+    m += 1
+print(f'Необходимо взять в долг {round(debt, 2)} рублей')
